43c6ec35162471829b7972bfe419f510fd116f11
konrad <konradkurowski028@gmail.com>
2025-02-17 16:01:20 +0100

    new

--- push_swap.h
+++ push_swap.h
@@ -15,4 +15,19 @@
 
 # include "libft.h"
 
+// struktura listy
+typedef struct s_node
+{
+    int             value;
+    struct s_node   *next;
+    struct s_node   *prev;
+} t_node;
+
+//struktura stosu
+typedef struct s_stack
+{
+    t_node  *top;
+    int     size;
+} t_stack;
+
 #endif
